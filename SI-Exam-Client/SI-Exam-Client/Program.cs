8f83a0751043ac9bc6cbbefd336a5d6f76e45193
Kimon Togrou <33865708+Cybont@users.noreply.github.com>
2020-12-06 16:31:05 +0100

    init c# code

---
+++ SI-Exam-Client/SI-Exam-Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SI_Exam_Client
+{
+    class Program
+    {
+        public static void CreateBooking()
+        {
+            bool hasPostedBooking = Requests.PostBooking();
+
+            if (hasPostedBooking)
+            {
+                Console.WriteLine("Booking has been created!");
+            }
+            else {
+                Console.WriteLine(
+                    "Booking request failed, you may receive a phone call shortly if we feel that we can help you book your room(s)"
+                    );
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateBooking();
+        }
+    }
+}
---
+++ SI-Exam-Client/SI-Exam-Client/Requests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI_Exam_Client
+{
+    public static class Requests
+    {
+        public static bool PostBooking() {
+            return false;
+        }
+    }
+}
